1856b60f435274d9394dc0cc8b76c4fcf972345c
jueal <thescourgeofdragon@163.com>
2020-05-09 23:13:53 +0800

    update 05.09

--- challenge_12/challenge_12.py
+++ challenge_12/challenge_12.py
@@ -1,10 +1,19 @@
 from PIL import Image
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+import io
 
 def main():
-    with open("evil2.gfx", 'r') as file:
+    with open("evil2.gfx", 'rb') as file:
         data = file.read()
-    
 
+    for i in range(5):
+        f = data[i::5]
+        img = Image.open(io.BytesIO(f))
+        img.show()
+
+# five image:
+# dis pro port ional ity(be crossed out)
 
 
 if __name__ == '__main__':
